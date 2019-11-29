fa2b351f30c6796e41e3c4b1b20860541075c1aa
Lukas Kmoch <lukaskmochit@gmail.com>
2019-11-29 01:18:01 +0100

    Add SOLID examples

---
+++ SOLID/AspNetCoreExamples.SOLID/readme.md
@@ -0,0 +1,2 @@
+﻿Bibliography
+https://ivanderevianko.com/wp-content/uploads/2013/10/Agile-Principles-Patterns-and-Practices-in-C.pdf
---
+++ examples/AspNetCoreExamples.DockerConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspNetCoreExamples.DockerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
