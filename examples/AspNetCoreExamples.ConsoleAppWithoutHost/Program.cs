18d591c8ebaba1ebec612c1a439aa812420df355
Lukas Kmoch <lukaskmochit@gmail.com>
2019-12-02 10:19:27 +0100

    Add labs

--- examples/AspNetCoreExamples.ConsoleAppWithoutHost/Program.cs
+++ examples/AspNetCoreExamples.ConsoleAppWithoutHost/Program.cs
@@ -15,8 +15,14 @@ namespace AspNetCoreExamples.ConsoleAppWithoutHost
                 .AddEnvironmentVariables()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", false, false)
-                .AddUserSecrets("0ebb5397-00de-4e6b-8edf-644c830d9613") // comment out to use appsettings.Production.json
-                .AddInMemoryCollection(new[] { KeyValuePair.Create("ASPNETCORE_ENVIRONMENT", "Production") }); // mock setting environment variable 
+                .AddUserSecrets("0ebb5397-00de-4e6b-8edf-644c830d9613"); // comment out to use appsettings.Production.json
+
+            // mock setting environment variable   or use set ASPNETCORE_ENVIRONMENT = Production dotnet run and
+            // Commands: 
+            // set ASPNETCORE_ENVIRONMENT=Production
+            // dotnet run
+            // echo %ASPNETCORE_ENVIRONMENT%
+            configurationBuilder.AddInMemoryCollection(new[] { KeyValuePair.Create("ASPNETCORE_ENVIRONMENT", "Production") }); // mock setting environment variable
 
             IConfiguration configuration = configurationBuilder.Build();
             var environment = configuration["ASPNETCORE_ENVIRONMENT"];
@@ -24,11 +30,17 @@ namespace AspNetCoreExamples.ConsoleAppWithoutHost
                 .AddJsonFile($"appsettings.{environment}.json", false, false);
 
             configuration = configurationBuilder.Build();
+
+            // var config = new AppSettings();
+            // configuration.Bind(AppSettings);
+
             var services = new ServiceCollection()
                 .AddTransient<ExampleApp>()
                 .AddTransient<Shop>()
                 .AddSingleton<Warehouse>() // AddTransient
-                .AddSingleton(configuration); // TODO Bind<AppSettings>
+                .AddSingleton(configuration);
+
+            services.AddSingleton(configuration);
 
             var serviceProvider = services
                 .BuildServiceProvider();
